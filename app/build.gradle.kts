a7a04cf746c16922102fb046dcce3131ff8f8b8c
Waldir <waldir.baia@atlantasistemas.com.br>
2025-07-30 15:55:17 -0300

    Implementando o hilt para realizar a injeções de dependencias.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.dagger.hilt)
 }
 
 android {
@@ -46,4 +47,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Dagger Hilt
+    implementation(libs.dagger.hilt)
+    ksp(libs.hilt.compiler)
 }
